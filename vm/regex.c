c8efa6280c4733d87541ae8d6e0277ac1ca2b9a9
Scott W. Dunlop <sdunlop@hydra>
2008-07-30 02:21:06 -0500

    Backported changes from Windows Compatibility tree.

--- vm/regex.c
+++ vm/regex.c
@@ -13,7 +13,7 @@
  * along with this library; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
-
+ 
 #include "waspvm.h"
 
 // Some old BSD regexes require that sys/types.h be included first.
